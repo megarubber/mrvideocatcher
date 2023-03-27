e572f7c1d4da9d3ebcc23137869f39c116309285
Guilherme Samuel <guisamuel800@gmail.com>
2023-03-26 23:40:20 -0300

    feat(download): add a alertdialog to control the file's name

--- app/src/main/java/com/mrguisamuel/mrvideocatcher/FileNameDialog.java
+++ app/src/main/java/com/mrguisamuel/mrvideocatcher/FileNameDialog.java
@@ -2,26 +2,57 @@ package com.mrguisamuel.mrvideocatcher;
 
 import android.app.AlertDialog;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class FileNameDialog {
+import java.util.concurrent.Callable;
 
-    public static String createFileNameDialog(AppCompatActivity myContext) {
-        String name = "";
+public class FileNameDialog {
+    public static String name = "";
+
+    public static void createFileNameDialog(
+            AppCompatActivity myContext,
+            Callable<Void> execute
+    ) {
         AlertDialog.Builder builder = new AlertDialog.Builder(myContext);
         builder.setTitle("Choose a file name");
 
-        final View customLayout = myContext.getLayoutInflater().inflate(R.drawable.name_alert);
+        View customLayout = myContext.getLayoutInflater().inflate(R.layout.name_alert, null);
         builder.setView(customLayout);
 
-        EditText inputName = (EditText) customLayout.findByViewId(R.id.file_name);
+        EditText inputName = customLayout.findViewById(R.id.file_name);
+        Button confirm = customLayout.findViewById(R.id.confirm);
+        Button cancel = customLayout.findViewById(R.id.cancel);
 
+        /*
         builder.setPositiveButton("Ok", ((dialogInterface, i) -> {
-
+            name = inputName.getText().toString();
         }));
+        */
+
         AlertDialog dialog = builder.create();
         dialog.show();
+
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name = inputName.getText().toString();
+                try {
+                    execute.call();
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+                dialog.dismiss();
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
     }
 }
--- app/src/main/java/com/mrguisamuel/mrvideocatcher/YouTubeDownloader.java
+++ app/src/main/java/com/mrguisamuel/mrvideocatcher/YouTubeDownloader.java
@@ -1,17 +1,33 @@
 package com.mrguisamuel.mrvideocatcher;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.concurrent.Callable;
+
 public class YouTubeDownloader extends AppCompatActivity {
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_youtube);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Download video from YouTube");
 
         Button videoDownload = (Button) findViewById(R.id.video);
@@ -21,7 +37,10 @@ public class YouTubeDownloader extends AppCompatActivity {
         videoDownload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //youtubeDownloader.downloadVideo(link.getText().toString());
+                FileNameDialog.createFileNameDialog(
+                    YouTubeDownloader.this,
+                    () -> { youtubeDownloader.downloadVideo(link.getText().toString(), "maisumteste"); return null; }
+                );
             }
         });
     }
